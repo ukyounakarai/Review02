10e7ee7f2ffd90f9b2506db9995442faafc75cd0
negan <negan@negaR13>
2023-02-09 19:46:58 +0900

    レッスン４の課題

---
+++ src/Review02.java
@@ -0,0 +1,23 @@
+
+public class Review02 {
+
+    public static void main(String[] args) {
+        //1から100までの連続した整数を画面に表示する
+        for (int i = 1; i <=100 ; i++) {
+          //上記2つの条件は同時に満たせる。つまり、もし、その整数が3で割り切れ、なおかつ5で割り切れる数なら "FizzBuzz" と表示する
+            if (i % 3 == 0&& i % 5 ==0)
+            System.out.println("FizzBuzz");{
+              //・もし、その整数が3で割り切れる数なら "Fizz" と表示する
+                if (i % 3 == 0)
+                System.out.println("Fizz");{
+                    if (i % 5 == 0)
+                    System.out.println("Buzz");
+                    //それ以外の数字を表示
+                    else System.out.println(i);
+                }
+            }
+        }
+
+    }
+
+}
