ef4121761cea121f185c9266fb78fff3a537d484
negan <negan@negaR13>
2023-02-09 22:14:30 +0900

    改善

--- src/Review02.java
+++ src/Review02.java
@@ -5,19 +5,16 @@ public class Review02 {
         //1から100までの連続した整数を画面に表示する
         for (int i = 1; i <=100 ; i++) {
           //上記2つの条件は同時に満たせる。つまり、もし、その整数が3で割り切れ、なおかつ5で割り切れる数なら "FizzBuzz" と表示する
-            if (i % 3 == 0&& i % 5 ==0)
-            System.out.println("FizzBuzz");{
+            if (i % 3 == 0&& i % 5 ==0) {
+            System.out.println("FizzBuzz");
               //・もし、その整数が3で割り切れる数なら "Fizz" と表示する
-                if (i % 3 == 0)
-                System.out.println("Fizz");{
-                    if (i % 5 == 0)
+            }else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                  //・もし、その整数が5で割り切れる数なら "Buzz" と表示する
+            } else if  (i % 5 == 0) {
                     System.out.println("Buzz");
                     //それ以外の数字を表示
-                    else System.out.println(i);
+        }else  System.out.println(i);
                 }
             }
-        }
-
-    }
-
 }
